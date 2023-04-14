5875aff6efbc197f0dce1defcb003b1a3918c2b2
Youssef Chaboune <Chahbouney2000@outlook.fr>
2023-04-14 10:37:42 -0400

    Added UI And logic for bullets Left
    Added All UI Elements and logic for bullets Left / bullet counter.

--- Assets/Player/Player Scripts/Shooting.cs
+++ Assets/Player/Player Scripts/Shooting.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 //using UnityEngine.Rendering.Universal;
 
 public class Shooting : MonoBehaviour
@@ -12,33 +13,48 @@ public class Shooting : MonoBehaviour
     public GameObject gunFire;
     public Animator anim;
 
+    public static int maxBulllets = 100;
+    public static int currentAmountOfBullet;
+    public Text bulletsLeftText;
+
     void Start()
     {
-        
+        currentAmountOfBullet = maxBulllets;
+        bulletsLeftText.text = currentAmountOfBullet.ToString() + " / " + maxBulllets.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        bulletsLeftText.text = currentAmountOfBullet.ToString() + " / " + maxBulllets.ToString();
+
         if (Input.GetMouseButtonDown(0))
         {
-            shootBullet();
-            gunFire.SetActive(true);
-            Invoke("setGunFireSctiveToFalse",0.1f);
-            anim.SetBool("isShooting", true);
-            Invoke("setIsShootingToFalse",0.1f);           
+            if (currentAmountOfBullet > 0)
+            {
+                shootBullet();
+                gunFire.SetActive(true);
+                Invoke("setGunFireSctiveToFalse", 0.1f);
+                anim.SetBool("isShooting", true);
+                Invoke("setIsShootingToFalse", 0.1f);
+            }
         }
-        
-        
+
+
         /*else if(Input.GetMouseButton(1))
         {
-            shootBullet();
+            GameObject bullet = Instantiate(bulletPrefab,firePoint.transform.position,firePoint.transform.rotation);
+            bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.transform.up * bulletSpeed, ForceMode2D.Impulse);
+            Destroy(bullet, 0.3f);
 
         }*/
     }
 
     public void shootBullet()
     {
+        currentAmountOfBullet -= 1;
+        bulletsLeftText.text = currentAmountOfBullet.ToString() + " / " + maxBulllets.ToString();
 
         GameObject bullet = Instantiate(bulletPrefab,firePoint.transform.position,firePoint.transform.rotation);
         bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.transform.up * bulletSpeed, ForceMode2D.Impulse);
