6f206babf6586fd00ac63616e197061fd7771a33
David Rallo <davidrallo28@gmail.com>
2023-04-14 14:05:26 -0400

    Update ChasePlayer.cs
    Figured out why it was flipping the zombie and adjusted it to face the player

--- Assets/Zombie/Zombie Scripts/ChasePlayer.cs
+++ Assets/Zombie/Zombie Scripts/ChasePlayer.cs
@@ -11,8 +11,8 @@ public class ChasePlayer : MonoBehaviour
     {
         // Move towards the player
         transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
-
-        
-        ;
+        Vector3 direction = playerTransform.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle - 90f);
     }
 }
