3e40d6d3e4fbd288221d8a07dedc7409b2d30c95
eb07 <EvanBond007@outlook.com>
2023-04-18 22:20:45 -0400

    Fixed Drop Spawner Script
    Script now randomly spawns a drop anywhere within the game's boundary

--- Assets/Drops/Drop Scripts/DropSpawner.cs
+++ Assets/Drops/Drop Scripts/DropSpawner.cs
@@ -4,47 +4,39 @@ using UnityEngine;
 
 public class DropSpawner : MonoBehaviour
 {
-    public GameObject[] drops; // an array of the 4 drops to be randomly selected
-    public float dropInterval = 30f; // the interval in seconds between drops
+    [SerializeField] GameObject[] dropPrefab; 
+    [SerializeField] float dropInterval = 30f;
+    [SerializeField] float minXTras;
+    [SerializeField] float maxXTras;
+    [SerializeField] float minYTras;
+    [SerializeField] float maxYTras;
 
-    private int lastDropIndex = -1; // the index of the last drop that was spawned
-    private bool isSpawning = false; // a flag indicating whether a drop is currently being spawned
+    int lastDropIndex = -1;
 
     void Start()
     {
-        StartCoroutine(SpawnDrops());
+        StartCoroutine(DropSpawn());
     }
 
-    IEnumerator SpawnDrops()
+    IEnumerator DropSpawn()
     {
-        while (true)
+        int dropIndex = Random.Range(0, dropPrefab.Length);
+        while(true)
         {
-            if (!isSpawning)
+            while (dropIndex == lastDropIndex)
             {
-                isSpawning = true;
-
-                // Choose a random drop index that is not the same as the last one
-                int dropIndex = Random.Range(0, drops.Length);
-                while (dropIndex == lastDropIndex)
-                {
-                    dropIndex = Random.Range(0, drops.Length);
-                }
-                lastDropIndex = dropIndex;
-
-                // Choose a random position for the drop
-                Vector3 dropPosition = new Vector3(Random.Range(-10f, 10f), 0f, Random.Range(-10f, 10f));
-
-                // Spawn the drop at the chosen position
-                Instantiate(drops[dropIndex], dropPosition, Quaternion.identity);
-
-                yield return new WaitForSeconds(dropInterval);
-
-                isSpawning = false;
+                dropIndex = Random.Range(0, dropPrefab.Length);
             }
-            else
-            {
-                yield return null;
-            }
+            lastDropIndex = dropIndex;
+
+            var spawnXPosition = Random.Range(minXTras, minYTras);
+            var spawnYPosition = Random.Range(minYTras, maxYTras);
+
+            var position = new Vector2(spawnXPosition, spawnYPosition);
+
+            GameObject gameObject = Instantiate(dropPrefab[dropIndex], position, Quaternion.identity);
+            Destroy(gameObject, 10f);
+            yield return new WaitForSeconds(dropInterval);
         }
     }
 }
