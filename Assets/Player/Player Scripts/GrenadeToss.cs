ffb40f2746fbe8037f88e22c95ffa3c5f6e82240
David Rallo <davidrallo28@gmail.com>
2023-04-19 11:08:00 -0400

    Continued on GrenadeScript
    Now a max amount of grenades

--- Assets/Player/Player Scripts/GrenadeToss.cs
+++ Assets/Player/Player Scripts/GrenadeToss.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GrenadeToss : MonoBehaviour
 {
-    public float grenadeSpeed=5f;
+    public float grenadeSpeed=50f;
     public GameObject grenadePrefab;
     public GameObject firePoint;
+    public static int maxGrenades = 3;
+    public static int currentAmountOfGrenades;
+    public Text grenadesLeftText;
     // Start is called before the first frame update
+
+     void Start()
+    {
+        currentAmountOfGrenades = maxGrenades;
+        grenadesLeftText.text =  "X " + currentAmountOfGrenades.ToString();
+    }
     void Update()
     {
+        grenadesLeftText.text = "X " + currentAmountOfGrenades.ToString();
         if (Input.GetMouseButtonDown(1))
         {
-            ThrowGrenade();
+            if (currentAmountOfGrenades > 0)
+            {
+                ThrowGrenade();
+            }
         }
     }
 
@@ -23,5 +37,6 @@ public class GrenadeToss : MonoBehaviour
 
         // Add force to the grenade to throw it forward
         rb.AddForce(firePoint.transform.up * grenadeSpeed, ForceMode2D.Impulse);
+        currentAmountOfGrenades--;
     }
 }
