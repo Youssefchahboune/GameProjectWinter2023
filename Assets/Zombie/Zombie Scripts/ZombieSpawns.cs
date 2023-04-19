3a557d13caee1f5e7d24ca669ee1afccb06337c6
David Rallo <davidrallo28@gmail.com>
2023-04-19 12:33:47 -0400

    Implemented Zombie Spawners
    Created all the spawners

--- Assets/Zombie/Zombie Scripts/ZombieSpawns.cs
+++ Assets/Zombie/Zombie Scripts/ZombieSpawns.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class ZombieSpawns : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject zombiePrefab;
+    [SerializeField]
+    private int minZombies = 1;
+    [SerializeField]
+    private int maxZombies = 3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Call SpawnZombies every 5 seconds
+        InvokeRepeating("SpawnZombies", 5f, 5f);
+    }
+
+    void SpawnZombies()
+    {
+        int numZombies = Random.Range(minZombies, maxZombies);
+
+        for (int i = 0; i < numZombies; i++)
+        {
+            Instantiate(zombiePrefab, transform.position, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
