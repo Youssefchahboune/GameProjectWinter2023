1232dadc38ecc0dbb0bb4bb182f8b31126785d29
eb07 <EvanBond007@outlook.com>
2023-04-19 11:20:00 -0400

    Nuke Script Updated
    On nuke pickup all zombie's get destroyed

--- Assets/Drops/Drop Scripts/Nuke.cs
+++ Assets/Drops/Drop Scripts/Nuke.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class Nuke : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             Destroy(gameObject);
+
+            GameObject[] zombies = GameObject.FindGameObjectsWithTag("Zombie");
+            foreach (GameObject zombie in zombies)
+            {
+                Destroy(zombie);
+            }
         }
     }
 }
