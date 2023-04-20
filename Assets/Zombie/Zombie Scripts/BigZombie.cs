5b2665f8557d9e03b6d726d6f45256987534063c
David Rallo <davidrallo28@gmail.com>
2023-04-20 09:11:46 -0400

    Created big zombie dies script

--- Assets/Zombie/Zombie Scripts/BigZombie.cs
+++ Assets/Zombie/Zombie Scripts/BigZombie.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private int maxHealth;
+    [SerializeField] private int currentHealth;
+    [SerializeField] private string bulletTag = "Bullet";
+
+    private List<SpriteRenderer> childSpriteRenderers = new List<SpriteRenderer>();
+    private List<Material> originalMaterials = new List<Material>();
+    public Material flashMaterial;
+
+
+    public static int countOfBigDeadZombies;
+
     void Start()
     {
-        
+        maxHealth = 12;
+        currentHealth = maxHealth;
+
+        foreach (Transform child in transform)
+        {
+            SpriteRenderer spriteRenderer = child.GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null)
+            {
+                childSpriteRenderers.Add(spriteRenderer);
+                originalMaterials.Add(spriteRenderer.material);
+            }
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag(bulletTag))
+        {
+            Destroy(collision.gameObject);
+
+            currentHealth--;
+
+            HitFlash();
+
+            if (currentHealth <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+    void Die()
+    {
+   
+        Destroy(gameObject);
+        Debug.Log("Total amount of big zombies dead: " + countOfBigDeadZombies);
+        Debug.Log("Total amount of zombies: " + ZombieSpawns.countOfTotalZombies);
+    }
+
+    public void HitFlash()
+    {
+        foreach (SpriteRenderer spriteRenderer in childSpriteRenderers)
+        {
+            spriteRenderer.material = flashMaterial;
+        }
+
+        Invoke("setPlayerMaterialToOriginal", 0.1f);
+    }
+
+    void setPlayerMaterialToOriginal()
+    {
+        for (int i = 0; i < childSpriteRenderers.Count; i++)
+        {
+            childSpriteRenderers[i].material = originalMaterials[i];
+        }
     }
 }
