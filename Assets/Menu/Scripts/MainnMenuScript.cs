4df6ad8421322e97bfd7676f65772f55cceacd9d
ctsaki25 <57469766+ctsaki25@users.noreply.github.com>
2023-05-04 04:40:16 -0400

    added images for tutorials and code

--- Assets/Menu/Scripts/MainnMenuScript.cs
+++ Assets/Menu/Scripts/MainnMenuScript.cs
@@ -9,13 +9,32 @@ public class MainMenuScript : MonoBehaviour
 {
     public TMP_InputField inputField;
     public GameObject startInputCanvas;
+    public GameObject askTutorialCanvas;
     public TMP_Text errorTextObject;
+
+    public GameObject tutorial1;
+    public GameObject tutorial2;
+    public GameObject tutorial3;
+    public GameObject tutorial4;
+    public GameObject tutorial5;
+    public GameObject tutorial6;
+
 
     void Start()
     {
         // Make sure the StartInput canvas is deactivated when the game starts
         startInputCanvas.SetActive(false);
         errorTextObject.gameObject.SetActive(false);
+        askTutorialCanvas.SetActive(false);
+
+        tutorial1.SetActive(false);
+        tutorial2.SetActive(false);
+        tutorial3.SetActive(false);
+        tutorial4.SetActive(false);
+        tutorial5.SetActive(false);
+        tutorial6.SetActive(false);
+
+
     }
 
     // Called when the "Start" button is clicked
@@ -37,7 +56,56 @@ public class MainMenuScript : MonoBehaviour
         else
         {
             PlayerPrefs.SetString("UserInput", userInput);
-            SceneManager.LoadScene("Game Scene");
+            askTutorialCanvas.SetActive(false);
+
         }
+    }
+
+
+    public void GoToTut1()
+    {
+        askTutorialCanvas.SetActive(false);
+        tutorial2.SetActive(false);
+        tutorial1.SetActive(true);
+    }
+
+    public void GoToTut2()
+    {
+        tutorial1.SetActive(false);
+        tutorial2.SetActive(true);
+        tutorial3.SetActive(false);
+    }
+
+    public void GoToTut3()
+    {
+        tutorial2.SetActive(false);
+        tutorial3.SetActive(true);
+        tutorial4.SetActive(false);
+    }
+
+    public void GoToTut4()
+    {
+        tutorial3.SetActive(false);
+        tutorial4.SetActive(true);
+        tutorial5.SetActive(false);
+    }
+
+    public void GoToTut5()
+    {
+        tutorial4.SetActive(false);
+        tutorial5.SetActive(true);
+        tutorial6.SetActive(false);
+    }
+
+    public void GoToTut6()
+    {
+        tutorial5.SetActive(false);
+        tutorial6.SetActive(true);
+    }
+
+    public void loadGame()
+    {
+        SceneManager.LoadScene("Game Scene");
+
     }
 }
