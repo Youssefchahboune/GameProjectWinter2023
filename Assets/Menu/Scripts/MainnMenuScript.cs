a689f92d680efc92ec852fd0a3b62f079658caf1
ctsaki25 <57469766+ctsaki25@users.noreply.github.com>
2023-05-04 04:19:54 -0400

    Added tutorials

--- Assets/Menu/Scripts/MainnMenuScript.cs
+++ Assets/Menu/Scripts/MainnMenuScript.cs
@@ -8,12 +8,36 @@ using UnityEngine.UI;
 public class MainMenuScript : MonoBehaviour
 {
     public TMP_InputField inputField;
+    public GameObject startInputCanvas;
+    public TMP_Text errorTextObject;
 
-    // Called when the "Save" button is clicked
+    void Start()
+    {
+        // Make sure the StartInput canvas is deactivated when the game starts
+        startInputCanvas.SetActive(false);
+        errorTextObject.gameObject.SetActive(false);
+    }
+
+    // Called when the "Start" button is clicked
+    public void StartGame()
+    {
+        // Activate the StartInput canvas when the Start button is clicked
+        startInputCanvas.SetActive(true);
+    }
+
+    // Called when the "Save" button is clicked in the StartInput canvas
     public void SaveInput()
     {
         string userInput = inputField.text;
-        PlayerPrefs.SetString("UserInput", userInput);
-        SceneManager.LoadScene("Game Scene");
+        if (string.IsNullOrEmpty(userInput))
+        {
+            // Activate the error text object if no text is entered
+            errorTextObject.gameObject.SetActive(true);
+        }
+        else
+        {
+            PlayerPrefs.SetString("UserInput", userInput);
+            SceneManager.LoadScene("Game Scene");
+        }
     }
 }
